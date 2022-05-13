298fbababda8e4ad2655b34b8a8f6adfaacd6bfa
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-05-13 11:43:33 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+#include <cstring>
 
+
+bool ricerca(char lista [10][20],char nome[1][20],int &pos){
+    for(int i=0;1<10;i++){
+        if(strcmp(lista[i],nome[0])==0){
+            pos=1;
+            return true;
+        }
+    }
+    return false;
+}
