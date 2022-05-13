ceb68a67617dad44f533132e6118c7b1109c147c
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-05-13 11:57:10 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,8 +2,8 @@
 #include <cstring>
 
 
-bool ricerca(char lista [10][20],char nome[1][20],int &pos){
-    for(int i=0;1<10;i++){
+bool ricerca(char lista [10][20], char nome[1][20],int &pos){
+    for(int i=0;i<10;i++){
         if(strcmp(lista[i],nome[0])==0){
             pos=1;
             return true;
